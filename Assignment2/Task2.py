2e894c75547a5178eff3c4a0f0f6b6236a96f1ea
JoyeetaBhowal <jaita.bhowal@gmail.com>
2021-03-17 22:17:27 -0400

    Task 2

--- Assignment2/Task2.py
+++ Assignment2/Task2.py
@@ -149,3 +149,75 @@ for x in range(7):
     if x == 3 or x == 6:
         continue
     print(x, end=' ')
+
+# 8.  Create a new dictionary by concatenating the following two dictionaries:
+# and letters. Sample input: consul72Expected output: Letters 6 Digits 2
+letters = 0
+digits = 0
+inp = input("Enter a string: ")
+for x in inp:
+    if x.isalpha():
+        letters += 1
+    elif x.isdigit():
+        digits += 1
+    else:
+        print("This is not a letter or a digit")
+print("Letters:", letters)
+print("Digits:", digits)
+
+# 9. Read the two parts of the question below:
+# Write a program such that it asks users to “guess the lucky number”.
+# If the correct number isguessed the program stops, otherwise it continues forever.
+# Modify the program so that it asks users whether they want to guess again each time.
+# Use two variables, ‘number’ for the number and ‘answer’ for the answer to the question
+# whether they want to continue guessing. The program stops if the user guesses the correct
+# number or answers “no”. (The program continues as long as a user has not answered “no” and
+# has not guessed the correct number)
+
+lucky_num = 10
+while True:
+    guess = int(input("Guess the lucky number: "))
+    if guess == lucky_num:
+        print("Good guess")
+        break
+    else:
+        answer = input("Do you want to continue? ")
+        if answer == "no":
+            break
+        else:
+            continue
+
+# 10. Write a program that asks five times to guess the lucky number. Use a while loop and a
+# counter,such as While counter <= 5:print(“Type in the”, counter, “number”counter=counter+1
+# The program asks for five guesses (no matter whether the correct number was guessed or not).
+# If the correct number is guessed, the program outputs “Good guess!”, otherwise it outputs “Try again!”.
+# After the fifth guess it stops and prints “Game over!”.
+count = 0
+lucky_num = 10
+while count <= 4:
+    guess = int(input("Guess the lucky number: "))
+    if guess == lucky_num:
+        print("Good guess")
+    else:
+        print("Try again")
+    count += 1
+    if count == 5:
+        print("Game over")
+        break
+
+
+#11. In the previous question, insert break after the “Good guess!” print statement. break will terminate
+# the while loop so that users do not have to continue guessing after they found the number.
+# If the user does not guess the number at all, print “Sorry but that was not very successful”.
+count = 0
+lucky_num = 10
+while count <= 4:
+    guess = int(input("Guess the lucky number: "))
+    if guess == lucky_num:
+        print("Good guess")
+        break
+    else:
+        print("Try again")
+    count += 1
+    if count == 5 and guess != lucky_num:
+        print("Sorry but that was not very successful")
